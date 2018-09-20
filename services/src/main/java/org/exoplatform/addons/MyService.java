9e4d2da08d19c82f68acfdac4339aead7cc05b63
mdiha <mdiha@exoplatform.com>
2018-09-20 09:58:58 +0100

    Added service

--- services/src/main/java/org/exoplatform/addons/MyService.java
+++ services/src/main/java/org/exoplatform/addons/MyService.java
@@ -1,9 +1,22 @@
 package org.exoplatform.addons;
+import org.exoplatform.services.log.ExoLogger;
+import org.picocontainer.Startable;
+import org.exoplatform.services.log.Log;
 
 /**
  * Created by eXo Platform SAS.
  *
  */
 
-public class MyService {
+public class MyService implements Startable {
+	private static final Log LOG = ExoLogger.getLogger(MyService.class);
+
+	public void start() {
+		LOG.info("Service started");
+	  }
+
+  
+  public void stop() {
+	  LOG.info("service stopped");
+  }
 }
