210ce924b41675c1641afb3a1efa5a5e7dec80c0
mdiha <mdiha@exoplatform.com>
2018-09-20 14:17:33 +0100

    Added simple service called by the Startable service

---
+++ services/src/main/java/org/exoplatform/addons/MySecondService.java
@@ -0,0 +1,15 @@
+package org.exoplatform.addons;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+
+public class MySecondService {
+    private static final Log LOG = ExoLogger.getLogger(MySecondService.class);
+
+    public void echo() {
+        LOG.info("Simple Service Started");
+
+    }
+}
+
+
+
--- services/src/main/java/org/exoplatform/addons/MyService.java
+++ services/src/main/java/org/exoplatform/addons/MyService.java
@@ -1,4 +1,6 @@
 package org.exoplatform.addons;
+import org.exoplatform.container.ExoContainer;
+import org.exoplatform.container.ExoContainerContext;
 import org.exoplatform.services.log.ExoLogger;
 import org.picocontainer.Startable;
 import org.exoplatform.services.log.Log;
@@ -9,14 +11,18 @@ import org.exoplatform.services.log.Log;
  */
 
 public class MyService implements Startable {
+	ExoContainer myContainer = ExoContainerContext.getCurrentContainer();
+	MySecondService myService = (MySecondService) myContainer.getComponentInstanceOfType(MySecondService.class);
+
 	private static final Log LOG = ExoLogger.getLogger(MyService.class);
 
 	public void start() {
-		LOG.info("Service started");
+		LOG.info("Service Started");
+		myService.echo();
+	  }
+  
+  	public void stop() {
+	  LOG.info("Service Stopped");
 	  }
 
-  
-  public void stop() {
-	  LOG.info("service stopped");
-  }
 }
