52886460ac38eaac2c6e22456536e804c2cbe9b5
mdiha <mdiha@exoplatform.com>
2018-09-21 10:32:07 +0100

    Added simple service called by the Startable service via constructor

--- services/src/main/java/org/exoplatform/addons/MyService.java
+++ services/src/main/java/org/exoplatform/addons/MyService.java
@@ -1,6 +1,4 @@
 package org.exoplatform.addons;
-import org.exoplatform.container.ExoContainer;
-import org.exoplatform.container.ExoContainerContext;
 import org.exoplatform.services.log.ExoLogger;
 import org.picocontainer.Startable;
 import org.exoplatform.services.log.Log;
@@ -11,18 +9,20 @@ import org.exoplatform.services.log.Log;
  */
 
 public class MyService implements Startable {
-	ExoContainer myContainer = ExoContainerContext.getCurrentContainer();
-	MySecondService myService = (MySecondService) myContainer.getComponentInstanceOfType(MySecondService.class);
-
+	MySecondService secondservice;
 	private static final Log LOG = ExoLogger.getLogger(MyService.class);
+
+	public  MyService (MySecondService mySecondService){
+		this.secondservice=mySecondService;
+	}
 
 	public void start() {
 		LOG.info("Service Started");
-		myService.echo();
-	  }
-  
-  	public void stop() {
-	  LOG.info("Service Stopped");
-	  }
+		secondservice.echo();
+	}
+
+	public void stop() {
+		LOG.info("Service Stopped");
+	}
 
 }
